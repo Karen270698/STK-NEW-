a2614769af32cd155780a05d8fa421043105cad2
Karen <karenjeday98@mail.ru>
2022-08-26 12:10:46 +0300

    first commit

---
+++ js/bgscript.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+/*--------------*/
+
+/* Когда пользователь нажимает на кнопку, переключаться раскрывает содержимое */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  // Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
---
+++ js/snow.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function ($) {
+
+    $(document).snowFlurry({
+        maxSize: 12,
+        numberOfFlakes: 80,
+        minSpeed: 15,
+        maxSpeed: 20,
+        color: 'Cornsilk',
+        timeout: 0
+    });
+
+});
